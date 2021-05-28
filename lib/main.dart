aef3cbbbb15b9e4a079899f0293f7ea46f51ca3f
nilsreichardt <nils@sharezone.net>
2021-05-28 15:06:23 +0200

    continued

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(App());
 
@@ -7,38 +6,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(builder: (context) {
-        return Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton(
-                    child: Text("Paste into Clipboard"),
-                    onPressed: () async {
-                      const flutterIsAwesome = "Flutter is awesome!";
-                      await Clipboard.setData(ClipboardData(text: flutterIsAwesome));
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("Pasted '$flutterIsAwesome' into clipboard"),
-                      ));
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text("Copy from Clipboard"),
-                    onPressed: () async {
-                      final clipboardData = await Clipboard.getData("text/plain");
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(clipboardData?.text ?? "null")),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      }),
+      home: Scaffold(
+        body: SafeArea(child: null),
+      ),
     );
   }
 }
