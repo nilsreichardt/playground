9a7d20da1909930246949c3149e6c9272af85c9a
nilsreichardt <nils@sharezone.net>
2021-08-11 13:23:01 +0200

    remove docs in `index.ts`

--- firebase-app-check-cloud-functions-unauthentificated/functions/src/index.ts
+++ firebase-app-check-cloud-functions-unauthentificated/functions/src/index.ts
@@ -1,8 +1,5 @@
 import * as functions from "firebase-functions";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
 export const helloWorld = functions.https.onCall(async (data, context) => {
     return "Working";
 });
