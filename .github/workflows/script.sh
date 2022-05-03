5382e9dd7ca34ebce568aebc98148a3e99f156d3
nilsreichardt <nils@reichardt.io>
2022-05-03 19:54:45 +0200

    Use own camera app

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -10,17 +10,15 @@ adb shell screenrecord /sdcard/recording.mp4 &
 PID=$!
 echo $PID
 
-# Download app
-curl -X GET -o "app.apk" "https://firebasestorage.googleapis.com/v0/b/nilsreichardt.appspot.com/o/mobile_scanner%2Fapp-release-2.apk?alt=media&token=3ff4fe70-3d23-4c7e-9849-edd622e7f7f1"
-
 # Install app
-adb install app.apk
+adb install example/app-release.apk
 
 # Grant permission
-adb shell pm grant dev.steenbakker.mobile_scanner_example android.permission.CAMERA
+adb shell pm grant com.example.example android.permission.CAMERA
+adb shell pm grant com.example.example android.permission.RECORD_AUDIO
 
 # Open app
-adb shell am start -n dev.steenbakker.mobile_scanner_example/io.flutter.embedding.android.FlutterActivity
+adb shell am start -n com.example.example/io.flutter.embedding.android.FlutterActivity
 
 # Make screenshots
 sleep 10
