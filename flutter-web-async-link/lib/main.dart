e25d457f98f37589787ea94a1476128ded536327
nilsreichardt <me@nils.re>
2023-10-13 19:07:59 +0200

    Setup async link example

---
+++ flutter-web-async-link/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Builder(builder: (context) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Demo of opening a link after awaiting a Future'),
+                const SizedBox(height: 12),
+                ElevatedButton(
+                  onPressed: () async {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Loading...'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+
+                    // Simulate loading
+                    await Future.delayed(const Duration(seconds: 2));
+
+                    launchUrl(
+                      Uri.parse('https://nils.re'),
+                      webOnlyWindowName: '_self',
+                    );
+                  },
+                  child: const Text('Open my website'),
+                ),
+              ],
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
